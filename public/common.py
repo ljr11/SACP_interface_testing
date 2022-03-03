7fbd06ce171060821883a49238f58cabb3dde264
ljr11 <3027507265@qq.com>
2022-03-03 10:41:39 +0800

    Optimize the testing framework

--- public/common.py
+++ public/common.py
@@ -9,10 +9,22 @@ import json
 import yaml
 def getYamlData():
     # 打开存好数据的yaml文件
-    file = open(r'D:\test-luban\luban\public\data.yaml')
+    file = open(r'D:\sacp\sacp_interface_testing\public\data.yaml')
     # 加载yaml数据，Loader=yaml.FullLoader只是为了不发出警告
     allData = yaml.load(file, Loader=yaml.FullLoader)
     file.close()
     return allData
-def common():
-    print("common")
+def simplereSponse():
+    f = open(getYamlData()['Path']['simplereSponse'], "r")
+    file = eval(f.read())
+    print(type(file))
+    arrs=[]
+    for key,value in file.items():
+        print(file[key]['gcode'])
+        arrs.append((file[key]['gcode'],file[key]['expect']))
+    f.close()
+    return(arrs)
+
+simplereSponse()
+def pushResponse():
+    print("pushResponse")
--- test_case/scap/test_sacp.py
+++ test_case/scap/test_sacp.py
@@ -1,30 +1,24 @@
 import unittest
-import os
-os.sys.path.append(r"D:\sacp\sacp_interface_testing\public")
 import time
 import json
 import serial
 from public.common import *
+from parameterized import parameterized
 class testupload(unittest.TestCase):
-    @classmethod
     def setUp(self):
-
-        portx = "COM3"  # COM2口用来读数
+        portx = "COM3"  # COM3口用来读数
         bps = 115200
         self.ser = serial.Serial(portx, bps)
     def tearDown(self):
-        pass
-    def test_upload_project_001(self):
-        '''上传3DP的工程文件'''
-        if self.ser.isOpen():  # 判断串口是否打开
-            print("open success")
-            self.ser.write("G53\n".encode("utf8"))
-            self.ser.write("G28\n".encode("utf8"))# 向端口些数据 字符串必须译码
-            time.sleep(3)
-            line = self.ser.read_all()
-            print(line)
-            self.ser.close()  # 关闭端串口
-        else:
-            print("open failed")
+        self.ser.close()  # 关闭端串口
+
+    @parameterized.expand(simplereSponse())
+    def test_upload_project_001(self,gcode,exceptation):
+        self.ser.write(gcode.encode("utf8"))
+        time.sleep(3)
+        line = str(self.ser.read_all(),encoding = "utf-8")
+        self.assertIn(exceptation, line)
+
+
 if __name__ == "__mian__":
     unittest.main()  # 主函数 入口
